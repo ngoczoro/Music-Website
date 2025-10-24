0e24c23042a7d42108acf3da7af76e9a3ac188e2
23521019-hub <147578270+ngoczoro@users.noreply.github.com>
2025-10-24 13:38:17 +0700

    p1 sidebar
    p1 sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.jsx";
+import { Sidebar } from "./components/custom/Sidebar.jsx";
 
-createRoot(document.getElementById('root')).render(
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Sidebar activeItem="Home" />
+  </StrictMode>
+);
