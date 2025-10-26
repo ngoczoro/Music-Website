987882cf184561b884ed9f5c1c4839f46d45908b
nmkhoi11042005 <23520777@gm.uit.edu.vn>
2025-10-26 17:24:30 +0700

    Update index.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../pages/Register";
 import VerifyEmail from "../pages/VerifyEmail";
 import ProfilePage from "../pages/ProfilePage";
+import MyPlaylist from "../pages/MyPlaylist";
 import MainLayout from "../layout/MainLayout";
 
 export default function AppRouter() {
@@ -12,6 +13,7 @@ export default function AppRouter() {
           <Route path="/" element={<Register />} />
           <Route path="/verify-email" element={<VerifyEmail />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/playlist" element={<MyPlaylist />} />
         </Routes>
       </MainLayout>
     </BrowserRouter>
