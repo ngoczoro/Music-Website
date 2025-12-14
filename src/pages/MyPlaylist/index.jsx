7ec9a05a5c18c7012d269aefb8328bd46d2a1bf3
nmkhoi11042005 <23520777@gm.uit.edu.vn>
2025-12-14 14:23:15 +0700

    Update index.jsx
    Translate a few things into English

--- src/pages/MyPlaylist/index.jsx
+++ src/pages/MyPlaylist/index.jsx
@@ -98,9 +98,9 @@ const MyPlaylist = () => {
 
         {/* ✅ Nhóm nút nằm bên phải */}
         <div className="playlist-action-buttons">
-          <button className="btn-primary" onClick={() => alert("TODO: Thêm playlist")}>➕ Thêm playlist mới</button>
-          <button className="btn-primary" onClick={() => alert("TODO: Sửa playlist")}>✏ Sửa playlist</button>
-          <button className="btn-primary btn-danger" onClick={() => alert("TODO: Xóa playlist")}>🗑 Xóa playlist</button>
+          <button className="btn-primary" onClick={() => alert("TODO: Thêm playlist")}>➕ Add new playlist</button>
+          <button className="btn-primary" onClick={() => alert("TODO: Sửa playlist")}>✏ Customize created playlist</button>
+          <button className="btn-primary btn-danger" onClick={() => alert("TODO: Xóa playlist")}>🗑 Delete created playlist</button>
         </div>
       </div>
 
