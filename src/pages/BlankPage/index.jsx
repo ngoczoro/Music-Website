bbe01c0b4446fba65d1e39a174ca4aefd19d0d58
TuUyen038 <uyentu510@gmail.com>
2025-12-17 00:38:33 +0700

    feat: requireAuth for guest

---
+++ src/pages/BlankPage/index.jsx
@@ -0,0 +1,17 @@
+// Home.jsx
+import React from "react";
+import "../../styles/theme.css";
+import HomeSearch from "../../components/custom/HomeSearch";
+import { TrendingList } from "../../components/custom/TrendingList";
+import { MadeForYouList } from "../../components/custom/MadeForYouList";
+import { TopArtistList } from "../../components/custom/TopArtistList";
+
+export default function Blank() {
+  return (
+    <div className="home-page">
+      <div className="home-content">
+      <h5>This page will be completed in the future</h5>
+      </div>
+    </div>
+  );
+}
